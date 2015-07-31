a78d243fa308236994935078fd77778a9a4ebb2a
王万霖 <dgideas@outlook.com>
2015-07-31 16:24:47 +0800

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -2,7 +2,7 @@ echo ===========================================================================
 echo ================================================================================
 echo ================================================================================
 echo DGideas CentOS7 Install Command Build July,2015
-echo + PHP + Python3 + Nodejs + sl + wget + gcc + ppp + pptpd
+echo + PHP + Nodejs + sl + wget + gcc + ppp + pptpd
 echo Powered By DGideas
 echo Thanks Github and sinaapp cloud!
 echo ================================================================================
@@ -10,39 +10,6 @@ yum install -y php > /dev/null 2>&1
 yum install -y sl > /dev/null 2>&1
 yum install -y wget > /dev/null 2>&1
 cd
-echo ================================================================================
-echo Getting Wget Python-3.3.0.tar.bz2 From DGideas GitHub Mirror...
-echo =====================Notifications==============================================
-echo + This step would take long time. It depends on your region and 
-echo   network settings.
-echo + This mirror is from DGideas/mirror. Thanks DGideas and Github!
-echo + Any questions? Please send email to dgideas@outlook.com
-echo ================================================================================
-wget https://github.com/DGideas/mirror/raw/master/Python-3.3.0.tar.bz2
-echo ================================================================================
-echo ===============================Download Successfully!!!=========================
-echo ===============================Upzip Package...Please wait======================
-tar -jxvf Python-3.3.0.tar.bz2 > /dev/null 2>&1
-rm -f Python-3.3.0.tar.bz2 > /dev/null 2>&1
-cd Python-3.3.0 > /dev/null 2>&1
-echo ===============================Configure Python...==============================
-./configure > /dev/null 2>&1
-echo ================================================================================
-echo Build Python...Please wait a moment, sit and relax...
-echo ================================================================================
-echo ===============================Make All...======================================
-make all > /dev/null 2>&1
-echo ===============================Make Install...==================================
-make install > /dev/null 2>&1
-echo ===============================Make Clean...====================================
-make clean > /dev/null
-echo ===============================Make Distclean...================================
-make distclean > /dev/null 2>&1
-/usr/local/bin/python3 -V
-echo ===============================Install Python 3 Successfully====================
-mv /usr/bin/python /usr/bin/python2.4
-ln -s /usr/local/bin/python3.3 /usr/bin/python
-python -V
 echo ================================================================================
 echo ===============================Install Vpn tools...=============================
 echo ================================================================================
@@ -61,6 +28,5 @@ echo ======You can copy this code to edit=======================================
 echo [     vi /usr/bin/yum                                                          ]
 echo ================================================================================
 echo Success > success.log
-rm -rf ~/Python-3.3.0/
 rm -f centos7.sh
 rm -f nohup.out

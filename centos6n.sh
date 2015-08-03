77cb146d721e2c1b8b54a452735ce30642a755e0
王万霖 <dgideas@outlook.com>
2015-08-03 12:27:17 +0800

    Update centos6n.sh

--- centos6n.sh
+++ centos6n.sh
@@ -9,6 +9,7 @@ echo ===========================================================================
 yum install -y php > /dev/null 2>&1
 yum install -y sl > /dev/null 2>&1
 yum install -y wget > /dev/null 2>&1
+yum install -y sysstat > /dev/null 2>&1
 cd
 echo ================================================================================
 echo Getting Wget Python-3.3.0.tar.bz2 From DGideas GitHub Mirror...
